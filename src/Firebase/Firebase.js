0b5c90d2511bc9de4f2ad7f4501ddb6ba1fe791d
Hafiz Jazim Farjad <jazim52946@gmail.com>
2024-08-19 20:25:55 +0500

    FireBase Update

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -6,28 +6,28 @@ import { getFirestore } from "firebase/firestore";
 
 // Hassan database credintials 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBt3RPQaKDUQ6iWcAjTeyvEMONgUkVh0dA",
-  authDomain: "accountingsoftware-fa9ab.firebaseapp.com",
-  projectId: "accountingsoftware-fa9ab",
-  storageBucket: "accountingsoftware-fa9ab.appspot.com",
-  messagingSenderId: "1388068342",  
-  appId: "1:1388068342:web:a22f20b1e88d73a0d15bd7",
-  measurementId: "G-PGRYQ81Y1B"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBt3RPQaKDUQ6iWcAjTeyvEMONgUkVh0dA",
+//   authDomain: "accountingsoftware-fa9ab.firebaseapp.com",
+//   projectId: "accountingsoftware-fa9ab",
+//   storageBucket: "accountingsoftware-fa9ab.appspot.com",
+//   messagingSenderId: "1388068342",  
+//   appId: "1:1388068342:web:a22f20b1e88d73a0d15bd7",
+//   measurementId: "G-PGRYQ81Y1B"
+// };
 
 
 // Jaizm database credintials hello
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAbAKpPuSYcUzeKauH4_hvh4R1ezRWKmVQ",
-//   authDomain: "account-bc9d2.firebaseapp.com",
-//   projectId: "account-bc9d2",
-//   storageBucket: "account-bc9d2.appspot.com",
-//   messagingSenderId: "141202899686",
-//   appId: "1:141202899686:web:c16fe23797f7ee60597127",
-//   measurementId: "G-8BGDZ01FGW"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyAbAKpPuSYcUzeKauH4_hvh4R1ezRWKmVQ",
+  authDomain: "account-bc9d2.firebaseapp.com",
+  projectId: "account-bc9d2",
+  storageBucket: "account-bc9d2.appspot.com",
+  messagingSenderId: "141202899686",
+  appId: "1:141202899686:web:c16fe23797f7ee60597127",
+  measurementId: "G-8BGDZ01FGW"
+};
 
 
 export const app = initializeApp(firebaseConfig);
